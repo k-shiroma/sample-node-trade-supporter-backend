64cb3520cdbd663c869b599acb832111bc9ce8c3
k.shiroma <kazutoshi.shiroma@gmail.com>
2021-11-07 13:13:20 +0000

    Items,Prices,Recipes,Materialsルーティング追加

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var rolesRouter = require('./routes/roles');
+var itemsRouter = require('./routes/items');
+var pricesRouter = require('./routes/prices');
+var recipesRouter = require('./routes/recipes');
+var materialsRouter = require('./routes/materials');
 
 var app = express();
 
@@ -18,5 +22,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/roles', rolesRouter);
+app.use('/items', itemsRouter);
+app.use('/prices', pricesRouter);
+app.use('/recipes', recipesRouter);
+app.use('/materials', materialsRouter);
 
 module.exports = app;
